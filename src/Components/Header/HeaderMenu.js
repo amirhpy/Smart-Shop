66b91421c6e0c618db844e17d9291b12c2b380cb
amir hpr <amirhosseinhphpam@gmail.com>
2023-02-08 12:28:30 +0330

    Test Succssfully

--- src/Components/Header/HeaderMenu.js
+++ src/Components/Header/HeaderMenu.js
@@ -4,7 +4,7 @@ const HeaderMenu = ({ isScroll }) => {
     return (
         <ul className='header__menu' style={{ color: isScroll ? '#000' : '#FFF' }}>
             <li className='header__menu-item'>Home</li>
-            <li className='header__menu-item'>TEsT Netlify</li>
+            <li className='header__menu-item'>Gallery</li>
             <li className='header__menu-item'>Features</li>
             <li className='header__menu-item'>Reviews</li>
             <li className='header__menu-item'>Shop</li>
