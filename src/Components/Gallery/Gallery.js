d3ffd4bc689897c48156b6ff183ed77df1909f30
amir hpr <amirhosseinhphpam@gmail.com>
2023-02-09 22:35:24 +0330

    fixed AboutProduct

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -9,12 +9,14 @@ import AboutProduct from '../shared/AboutProduct';
 const Gallery = () => {
     return (
         <div className='gallery'>
-            <AboutProduct
-                heading='ABOUT OUR WATCHES'
-                fTitle='New, Improved'
-                lTitle='Design and Performance'
-                more='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae odales lectus, non ultrices nisi. In varius, leo non gravida.' />
             <div className='container'>
+                <div style={{ textAlign: 'center' }}>
+                    <AboutProduct
+                        heading='ABOUT OUR WATCHES'
+                        fTitle='New, Improved'
+                        lTitle='Design and Performance'
+                        more='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae odales lectus, non ultrices nisi. In varius, leo non gravida.' />
+                </div>
                 <GallerySlider />
             </div>
         </div>
