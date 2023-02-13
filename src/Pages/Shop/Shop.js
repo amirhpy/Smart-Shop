dbe7fa34f079e1f5c3e3748afbd93ac6f073462e
amir hpr <amirhosseinhphpam@gmail.com>
2023-02-13 08:42:21 +0330

    fix bug

---
+++ src/Pages/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-3'></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
---
+++ src/Pages/Shop/allProduct.js
@@ -0,0 +1,12 @@
+let allProducts = [
+    {id: 1, name: '', information: '', price: '', img: ''},
+    {id: 2, name: '', information: '', price: '', img: ''},
+    {id: 3, name: '', information: '', price: '', img: ''},
+    {id: 4, name: '', information: '', price: '', img: ''},
+    {id: 5, name: '', information: '', price: '', img: ''},
+    {id: 6, name: '', information: '', price: '', img: ''},
+    {id: 7, name: '', information: '', price: '', img: ''},
+    {id: 8, name: '', information: '', price: '', img: ''},
+]
+
+export default allProducts
