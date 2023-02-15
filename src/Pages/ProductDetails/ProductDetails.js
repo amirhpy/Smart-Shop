4919437ef12558efb89ca4005a185c3fe7c2c15a
amir hpr <amirhosseinhphpam@gmail.com>
2023-02-15 14:09:54 +0330

    compeleted JSX ProductDetails and change Quality img Smarts

--- src/Pages/ProductDetails/ProductDetails.js
+++ src/Pages/ProductDetails/ProductDetails.js
@@ -7,23 +7,56 @@ import allProducts from '../Shop/allProduct';
 // Components
 import PaginationShop from '../../Components/shared/PaginationShop';
 
+// React Icon
+import { RxArrowUp } from 'react-icons/rx'
+import { RxArrowDown } from 'react-icons/rx'
+import { BsBasket } from 'react-icons/bs'
+
 const ProductDetails = () => {
     let params = useParams()
     const [allProduct] = useState(allProducts)
     let mainProduct = allProduct.find(prod => prod.id === Number(params.productID))
-    
+
     return (
         <div className='product-details'>
             <PaginationShop />
             <div className='container'>
                 <div className='main__product'>
                     <div className='row'>
-                        <div className='col-6'>
-                            <div className='mainprod__img-container'>
-                                <img src={mainProduct.img} alt='smart' className='mainprod__img' />
+                        <div className='col-12 col-lg-6'>
+                            <div className='main-prod__imgcontainer'>
+                                <img src={mainProduct.img} alt='smart' className='main-prod__img' />
                             </div>
                         </div>
-                        <div className='col-6'></div>
+                        <div className='col-12 col-lg-6'>
+                            <div className='main-prod__body'>
+                                <h2 className='main-prod__name'>{mainProduct.name}</h2>
+                                <h3 className='main-prod__price'>£{mainProduct.price}</h3>
+                                <p className='main-prod__desc'>
+                                    Aenean egestas ante non ullamcorper rutrum. Donec fermentum mi nec massa tempor vehicula. Nulla nec porta lorem, laoreet congue lectus. Nullam commodo nisl in elit posuere, eget pulvinar nisl fringilla.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum velit id eros finibus luctus.
+                                </p>
+                                <div className='main-prod__basket'>
+                                    <div className='main-prod__counter'>
+                                        <div className='counter-up'>
+                                            <RxArrowUp />
+                                        </div>
+                                        <p className='counter-show'>{mainProduct.count}</p>
+                                        <div className='counter-down'>
+                                            <RxArrowDown />
+                                        </div>
+                                    </div>
+                                    <div className='main-prod__button'>
+                                        <BsBasket />
+                                        <p>Add To Cart</p>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+                                    <h3 className='main-prod__category'>Category :</h3>
+                                    <p className='shop__product-info pb-0'>{mainProduct.info}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/Shop/allProduct.js
+++ src/Pages/Shop/allProduct.js
@@ -8,14 +8,14 @@ import smartSkyBlue from '../../assets/image/shop-smart/watch-skyblue.jpg'
 import smartSnowWhite from '../../assets/image/shop-smart/watch-snowwhite.jpg'
 
 let AllProducts = [
-    { id: 1, name: 'Charcoal Black', info: 'strap', price: '25.50', img: smartCharcoalBlack },
-    { id: 2, name: 'Golden Sunset', info: 'strap', price: '25.50', img: smartGoldenSunset },
-    { id: 3, name: 'Light Pink', info: 'strap', price: '25.50', img: smartLightPink },
-    { id: 4, name: 'Mint Green', info: 'strap', price: '25.50', img: smartMintGreen },
-    { id: 5, name: 'Pink Lemonade', info: 'strap', price: '25.50', img: smartPinkLemonade },
-    { id: 6, name: 'Purple Berry', info: 'strap', price: '25.50', img: smartPurpleBerry },
-    { id: 7, name: 'Sky Blue', info: 'strap', price: '25.50', img: smartSkyBlue },
-    { id: 8, name: 'Snow White', info: 'strap', price: '25.50', img: smartSnowWhite }
+    { id: 1, count: 1, name: 'Charcoal Black', info: 'strap', price: '25.50', img: smartCharcoalBlack },
+    { id: 2, count: 1, name: 'Golden Sunset', info: 'strap', price: '25.50', img: smartGoldenSunset },
+    { id: 3, count: 1, name: 'Light Pink', info: 'strap', price: '25.50', img: smartLightPink },
+    { id: 4, count: 1, name: 'Mint Green', info: 'strap', price: '25.50', img: smartMintGreen },
+    { id: 5, count: 1, name: 'Pink Lemonade', info: 'strap', price: '25.50', img: smartPinkLemonade },
+    { id: 6, count: 1, name: 'Purple Berry', info: 'strap', price: '25.50', img: smartPurpleBerry },
+    { id: 7, count: 1, name: 'Sky Blue', info: 'strap', price: '25.50', img: smartSkyBlue },
+    { id: 8, count: 1, name: 'Snow White', info: 'strap', price: '25.50', img: smartSnowWhite }
 ]
 
 export default AllProducts
