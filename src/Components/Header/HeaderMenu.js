875282c55c29d357921d30e30e1ea0473a11c1f3
amirhossein <amirhosseinhphpam@gmail.com>
2023-06-25 08:48:33 +0330

    add react-scroll

--- src/Components/Header/HeaderMenu.js
+++ src/Components/Header/HeaderMenu.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-// Components
-import MoveSectionMenuLi from './MoveSectionMenuLi';
+import * as Scroll from 'react-scroll'
 
 const HeaderMenu = ({ isScroll, menuOpen }) => {
-    const [menuList] = useState([
-        { id: 1, className: 'header__menu-item', dataSection: 'gallery', innerTag: 'Gallery' },
-        { id: 2, className: 'header__menu-item', dataSection: 'features', innerTag: 'Features' },
-        { id: 3, className: 'header__menu-item', dataSection: 'review', innerTag: 'Reviews' }
-    ])
-
     return (
         <ul className={menuOpen} style={{ color: isScroll ? '#000' : '#FFF' }}>
             <li className='header__menu-item'>
                 <Link to='/'>Home</Link>
             </li>
-            {menuList.map(li => <MoveSectionMenuLi key={li.id} {...li} />)}
+            <li className='header__menu-item'>
+                <Scroll.Link
+                    to='gallery'
+                    smooth={true}
+                >
+                    Gallery
+                </Scroll.Link>
+            </li>
+            <li className='header__menu-item'>
+                <Scroll.Link
+                    to='features'
+                    smooth={true}
+                >
+                    Features
+                </Scroll.Link>
+            </li>
+            <li className='header__menu-item'>
+                <Scroll.Link
+                    to='review'
+                    smooth={true}
+                >
+                    Reviews
+                </Scroll.Link>
+            </li>
             <li className='header__menu-item'>
                 <Link to='/shop'>Shop</Link>
             </li>
