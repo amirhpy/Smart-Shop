e7349fcd5e0802b8f899062778e866b538b70963
amirhossein <amirhosseinhphpam@gmail.com>
2023-06-25 08:59:42 +0330

    fix scroll

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import CartContextProvider from './Contexts/CartContextProvider'
 // Component
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
 
 // Routes
-import routes from './routes';
+import routes from './routes'
 
 function App() {
     let router = useRoutes(routes)
     return (
-        <AllProductsContextProvider>
-            <CartContextProvider>
-                <Header />
-                {router}
-                <Footer />
-            </CartContextProvider>
-        </AllProductsContextProvider>
-
+        <>
+            <AllProductsContextProvider>
+                <CartContextProvider>
+                    <Header />
+                    {router}
+                    <Footer />
+                </CartContextProvider>
+            </AllProductsContextProvider>
+            <ScrollToTop />
+        </>
     );
 }
 
